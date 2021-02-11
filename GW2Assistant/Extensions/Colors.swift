1c37cad94268ca184b38b99afd175373ddd2bc8d
Gustavo Lemos <gsvieira08@gmail.com>
2021-02-11 16:09:23 -0300

    Colors

---
+++ GW2Assistant/Extensions/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  GW2Assistant
+//
+//  Created by Gustavo Lemos on 11/02/21.
+//
+
+import UIKit
+
+class Colors: UIColor {
+
+}
---
+++ GW2Assistant/View/Components/MenuButton.swift
@@ -0,0 +1,20 @@
+//
+//  MenuButton.swift
+//  GW2Assistant
+//
+//  Created by Gustavo Lemos on 11/02/21.
+//
+
+import UIKit
+
+class MenuButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
