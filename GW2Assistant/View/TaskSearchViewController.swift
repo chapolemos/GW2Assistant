85ce56a9aafcef70115ce5a99f188035c4f0448e
Gustavo Lemos <gsvieira08@gmail.com>
2021-02-11 13:59:12 -0300

    Re-init commit

---
+++ GW2Assistant/View/TaskSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskSearchViewController.swift
+//  GW2Assistant
+//
+//  Created by Gustavo Lemos on 11/02/21.
+//
+
+import UIKit
+
+class TaskSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
